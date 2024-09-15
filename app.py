a6d3582e68499a98ce1ca8b9a2564d2bf9c2f608
vsewolod.schmidt@sap.com <vsewolod.schmidt@sap.com>
2024-09-15 15:06:07 +0200

    update 4 files and delete 8 files

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import taipy as tp
-from taipy.gui import notify, Gui, State
+from taipy.gui import notify, Gui, State, navigate
 from taipy import Config
 import pandas as pd
 import datetime as dt
 import os
 from modules.utils import log
 
-Config.load('config.toml')  # Updated to use config.toml
+Config.load("config.toml")  # Updated to use config.toml
 
 try:
     if selected_nodes:
@@ -17,6 +17,7 @@ except:
     selected_nodes = []
     selected_jobs = []
 
+
 def on_init(state: State):
     log("Initializing application state.")
     state.date = None
@@ -24,30 +25,53 @@ def on_init(state: State):
 
 
 def on_data_node_change(state: State, var_name, var_value):
-    log(f"on_data_node_change triggered with var_name={var_name}, var_value={var_value}")
+    log(
+        f"on_data_node_change triggered with var_name={var_name}, var_value={var_value}"
+    )
     state.selected_nodes = var_value
     notify(state, f"Selected data nodes updated: {state.selected_nodes}", "success")
 
-html = """
-<|layout|columns=150px auto|
-<|{scenario}|scenario_selector|>
-<|part|
-<|layout|columns=450px auto|
-<|{scenario}|scenario|>
-<|part|
-<|{scenario}|scenario_dag|>
-|>
-|>
-|>
-|>
+
+root_md = """
+<|navbar|>
+"""
+
+job_selector_md = """
+<|layout|columns=1|fullheight|
 <|{selected_jobs}|job_selector|>
-<|layout|columns=1 1|
-<|{scenario}|data_node_selector|on_change=on_data_node_change|>
-<|{selected_nodes}|data_node|>
 |>
 """
 
-gui = Gui(page=html)
+data_node_view_md = """
+<|layout|columns=300px 1|class_name=context_container|
+<|part|
+<|{scenario}|data_node_selector|on_change=on_data_node_change|>
+|>
+<|part|
+<|{selected_nodes}|data_node|show_config=True|>
+|>
+|>
+"""
+
+scenario_configure_md = """
+<|layout|columns=300px 1|
+<|part|class_name=scenario_selector_container|
+<|{scenario}|scenario_selector|align_item_bottom|>
+|>
+<|{scenario}|scenario|>
+<|{scenario}|scenario_dag|show_toolbar=False|class_name=graph_container|>
+|>
+"""
+
+def on_menu(state, action, info):
+    page = info["args"][0]
+    navigate(state, to=page)
+
+
+pages = {"/": root_md, "Scenarios": scenario_configure_md, "Data": data_node_view_md, "Jobs": job_selector_md}
+
+
+gui = Gui(css_file="modules/styles.css", pages=pages)
 gui.run(
     on_init=on_init,
     port=5001,
@@ -55,7 +79,6 @@ gui.run(
     dark_mode=True,
     use_reloader=True,
     allow_unsafe_werkzeug=True,
-    async_mode='threading',
+    async_mode="threading",
     debug=True,
 )
-
